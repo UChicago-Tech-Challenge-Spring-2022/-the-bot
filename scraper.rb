66d2dc3ae2fd67ef8e8baf1a01ef04258331ab3e
liu-yilan <yilan767385@gmail.com>
2022-05-08 07:00:00 +0000

    created scraper.rb file

---
+++ scraper.rb
@@ -0,0 +1,6 @@
+# precautionary measure: requiring open-uri and net/http
+
+require 'open-uri'
+require 'net/http'
+
+# https://coursesearch92.ais.uchicago.edu/psc/prd92guest/EMPLOYEE/HRMS/c/UC_STUDENT_RECORDS_FL.UC_CLASS_SEARCH_FL.GBL
